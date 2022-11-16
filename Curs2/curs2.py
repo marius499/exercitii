493ae46629940bf70503539509c58847fb4508e1
marius499 <75045503+marius499@users.noreply.github.com>
2022-11-16 15:39:55 +0200

    Update curs2.py

--- Curs2/curs2.py
+++ Curs2/curs2.py
@@ -1,3 +1,9 @@
+"""Implementare a unei clase numita Login care mosteneste unittest.TestCase
+selectori folositi : XPATH
+vom testa anumite functionalitati din pagina: https://the-internet.herokuapp.com/
+"""
+
+
 import unittest
 from time import sleep
 from selenium import webdriver
@@ -34,28 +40,28 @@ class Login(unittest.TestCase):
         self.chrome.implicitly_wait(5)
         self.chrome.find_element(*self.FORM_AUTH).click()
 
-    # se ruleaza dupa fiecare test
+    # se ruleaza dupa fiecare test si se inchde pagina dupa ce testul a fost terminat
     def tearDown(self):
         self.chrome.quit()
 
-    # Test1 - Verificati ca noul url e corect
+    # Test1 - se verifica ca noul url e corect
     def test_url(self):
         actual = self.chrome.current_url
         expected = 'https://the-internet.herokuapp.com/login'
         self.assertEqual(expected, actual, ' Site-ul nu e corect')
 
-    #Test2 - Verificati ca page title e corect
+    #Test2 - Se verifica daca titlul paginii este corect
     def test_page_title(self):
         actual = self.chrome.title
         expected = 'The Internet'
         self.assertEqual(expected, actual, 'Title is not correct')
 
-    #Test3 - Verificati textul de pe elementul xpath=//h2 e corect
+    #Test3 -textul de pe elementul xpath=//h2 e corect
     def test_elem_vizibil(self):
         elem = self.chrome.find_element(*self.LOGIN_PAGE_MSG)
         self.assertTrue(elem.is_displayed(), 'Message is not displayed')
 
-    #Test4 - Verificati ca butonul de login este displayed
+    #Test4 -  ca butonul de login este displayed
     def test_login_button(self):
         elem = self.chrome.find_element(*self.LOGIN_BUTTON)
         self.assertTrue(elem.is_displayed(), 'Button is not there')
@@ -72,14 +78,14 @@ class Login(unittest.TestCase):
         expected = 'Elemental Selenium'
         self.assertEqual(expected, actual, 'Attribute is different')
 
-    #Test6 - Lasati goale user si pass  /Click login / Verifica ca eroarea e displayed
+    #Test6 - se lasa goale  fieldurile de user si pass  > Click login  > Verifica ca eroarea e afisata
 
     def test_error_login_check(self):
         self.chrome.find_element(*self.LOGIN_BUTTON).click()
         error = self.chrome.find_element(*self.ERROR_MESSAGE)  # salvam eroare in error
         self.assertTrue(error.is_displayed(), 'Eroarea nu e vizibila')
 
-    #Test7 - Completeaza cu user si pass invalide/Click login / Verifica ca eroarea e displayed
+    #Test7 - Completeaza cu user si pass invalide > Click login >  Verifica ca eroarea e afisata
     def test_login_invalid(self):
         self.chrome.find_element(*self.USERNAME_LOGIN).send_keys('marius')
         self.chrome.find_element(*self.PASSWORD_LOGIN).send_keys('Test123')
@@ -89,16 +95,16 @@ class Login(unittest.TestCase):
         expected = 'Your username is invalid!'
         self.assertTrue(expected in actual, 'Error message text is incorrect')
 
-    #Test8 - Lasati goale user si pass  /Click login / Apasa x la eroare/ Verifica ca eroarea a disparut
+    #Test8 - se lasa goale  fieldurile de user si pass  > Click login > Apasa x la eroare > Verifica ca eroarea a disparut
     def test_error_login_check_errorIsGone(self):
         self.chrome.find_element(*self.LOGIN_BUTTON).click()
         self.chrome.find_element(*self.CLOSE_ERROR_MESSAGE).click()
         error = self.chrome.find_elements() # salvam eroare in error
         self.assertTrue(len(error) == 0, 'eroarea e vizibila')
-        # merge, dar nu stiu de ce :(  /// nu inteleg ce cauta acum rand 90 ... teoretic erooori pe care nu la gaseste, darr...
 
-    #Test9 - Ia ca o lista toate //label  /  Verifica textul ca textul de pe ele sa fie cel asteptat (Username si Password)
-    #         Aici e ok sa avem 2 asserturi
+
+    #Test9 - se ia ca o lista toate //label  >  se verifica textul; trebuie sa fie cel asteptat (Username si Password)
+
     def test_labels(self):
         actual = self.chrome.find_elements(*self.LABELS)[0].text
         expected = 'Username'
@@ -107,7 +113,7 @@ class Login(unittest.TestCase):
         expected = 'Password'
         self.assertEqual(expected, actual, 'label2 text  is wrong')
 
-    #Test10 - Completeaza cu user si pass valide  /  Click login  /  Verifica ca noul url CONTINE /secure
+    #Test10 - Completeaza cu user si pass valide  >  Click login  >  Verifica ca noul url CONTINE /secure
     #         Verifica ca elementul cu clasa=’flash succes’ este displayed
     #         Verifica ca mesajul de pe acest element CONTINE textul ‘secure area!’
     def test_login_pass(self):
@@ -120,7 +126,6 @@ class Login(unittest.TestCase):
         self.assertEqual(expected, actual, 'URL nu e corect')
         #self.chrome.find_elements(By.PARTIAL_LINK_TEXT, '/secure')[1].click()  # V2 Verifica ca noul url CONTINE /secure
         # clasa flash success e displayed
-       # WebDriverWait(self.chrome, 10).until(EC.presence_of_element_located(By.XPATH, '//*[@id="flash"]/text()'))  nu merge ????
         sucessMessage = self.chrome.find_element(*self.CORRECT_LOGIN_MESSAGE)  # salvam eroare in error
         self.assertTrue(sucessMessage.is_displayed(), 'Mesajul de succes nu s-a afisat')
         # partial text message is displayed
@@ -128,7 +133,7 @@ class Login(unittest.TestCase):
         expected = 'You logged into a secure area!'
         self.assertTrue(expected in actual, 'Error message text is incorrect')
 
-    #Test11  - Completeaza cu user si pass valide / Click login  / Click logout
+    #Test11  - Completeaza cu user si pass valide > Click login  > Click logout
     #          Verifica ca ai ajuns pe https://the-internet.herokuapp.com/login
     def test_login_pass_exit(self):
         self.chrome.find_element(*self.USERNAME_LOGIN).send_keys('tomsmith')
